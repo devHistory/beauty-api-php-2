79cfed51c8a6c9108f22cee31140f8eda55f0e76
Joe <joe@xxtime.com>
2017-09-25 16:49:29 +0800

    Relation

--- app/v1/controllers/RelationController.php
+++ app/v1/controllers/RelationController.php
@@ -4,18 +4,76 @@
 namespace MyApp\V1\Controllers;
 
 
+use MyApp\V1\Models\Relation;
+
 class RelationController extends ControllerBase
 {
+
+    private $relationModel;
+
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->relationModel = new Relation();
+    }
+
 
     // 关注
     public function followAction()
     {
+        $uid = $this->request->get('uid', 'alphanum', '');
+
+        $this->relationModel->follow($uid, $this->uid);
+
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success')
+        ])->send();
     }
 
 
     // 取消关注
     public function unfollowAction()
     {
+        $uid = $this->request->get('uid', 'alphanum', '');
+
+        $this->relationModel->unfollow($uid, $this->uid);
+
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success')
+        ])->send();
+    }
+
+
+    // 粉丝列表
+    public function followersAction()
+    {
+        $data = $this->relationModel->listFollowers($this->uid);
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success'),
+            'data' => [
+                'count' => count($data),
+                'list'  => $data
+            ]
+        ])->send();
+    }
+
+
+    // 关注列表
+    public function followingAction()
+    {
+        $data = $this->relationModel->listFollowing($this->uid);
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success'),
+            'data' => [
+                'count' => count($data),
+                'list'  => $data
+            ]
+        ])->send();
     }
 
 }
---
+++ app/v1/models/Relation.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MyApp\V1\Models;
+
+
+use Phalcon\Mvc\Model;
+use Phalcon\DI;
+use Phalcon\Db;
+
+class Relation extends Model
+{
+
+
+    /**
+     * @param string $following 关注对象
+     * @param string $uid 粉丝
+     * @return bool
+     */
+    public function follow($following = '', $uid = '')
+    {
+        $key = 'followers|' . $following;
+        $this->di['redis']->sAdd($key, $uid);
+
+        $key = 'following|' . $uid;
+        $this->di['redis']->sAdd($key, $following);
+
+        return true;
+    }
+
+
+    /**
+     * @param string $following 关注对象
+     * @param string $uid 粉丝
+     * @return bool
+     */
+    public function unFollow($following = '', $uid = '')
+    {
+        $key = 'followers|' . $following;
+        $this->di['redis']->sRem($key, $uid);
+
+        $key = 'following|' . $uid;
+        $this->di['redis']->sRem($key, $following);
+
+        return true;
+    }
+
+
+    // 粉丝列表
+    public function listFollowers($uid = '')
+    {
+        $key = 'followers|' . $uid;
+        $result = $this->di['redis']->sMembers($key);
+        return $this->getMoreInfo($result);
+    }
+
+
+    // 关注列表
+    public function listFollowing($uid = '')
+    {
+        $key = 'following|' . $uid;
+        $result = $this->di['redis']->sMembers($key);
+        return $this->getMoreInfo($result);
+    }
+
+
+    private function getMoreInfo($uidList = [])
+    {
+    }
+
+}
