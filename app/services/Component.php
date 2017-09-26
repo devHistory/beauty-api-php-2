b541d9f4468b73d0958905d854aad1fb05e1d5f4
Joe <joe@xxtime.com>
2017-09-26 19:20:14 +0800

    Component - Rank

--- app/services/Component.php
+++ app/services/Component.php
@@ -18,4 +18,24 @@ class Component
         return $this->di[$name];
     }
 
+
+    /**
+     * 排行榜
+     * rankHomeView | rankReward | rankContribute | rankPostLike | rankPostComment | rankPostView
+     *
+     * 例:
+     * 累计贡献 rank(rankContribute, $FromUid, $score);
+     * 详细贡献 rank(rankContribute|<toUid>, $FromUid, $score);
+     *
+     * @param string $key
+     * @param string $id
+     * @param int $score
+     * @return mixed
+     */
+    public function rank($key = '', $id = '', $score = 1)
+    {
+        return $this->redis->zIncrBy($key, $score, $id);
+        // TODO :: 周排行
+    }
+
 }
