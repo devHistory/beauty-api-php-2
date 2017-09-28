dbdf7f55ce4cd277e8a9035d17e42b66a6fa8822
Joe <joe@xxtime.com>
2017-09-28 17:48:27 +0800

    Collection module

--- app/v1/controllers/CollectionController.php
+++ app/v1/controllers/CollectionController.php
@@ -4,18 +4,88 @@
 namespace MyApp\V1\Controllers;
 
 
+use MyApp\V1\Models\Collection;
+
 class CollectionController extends ControllerBase
 {
 
-    // 收藏
+    private $collectionModel;
+
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->collectionModel = new Collection();
+    }
+
+
     public function indexAction()
     {
+        $type = $this->request->get('type', 'alphanum');
+
+        if (!in_array($type, ['post'])) {
+            return $this->response->setJsonContent(['code' => 1, 'msg' => _('parameter error')])->send();
+        }
+
+        $data = $this->collectionModel->listCollection($type, $this->uid);
+        $data = $this->component->fillUserByKey($data[$type], 'uid', ['name']);
+
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success'),
+            'data' => [
+                'count' => count($data),
+                'list'  => $data
+            ]
+        ])->send();
+    }
+
+
+    // 收藏
+    public function addAction()
+    {
+        $id = $this->request->get('id', 'alphanum');
+        $type = $this->request->get('type', 'alphanum');
+
+        // check
+        if (!$id || !$type || !in_array($type, ['post'])) {
+            return $this->response->setJsonContent(['code' => 1, 'msg' => _('parameter error')])->send();
+        }
+
+        // add
+        if (!$this->collectionModel->addCollection($type, $id, $this->uid)) {
+            return $this->response->setJsonContent(['code' => 1, 'msg' => _('fail')])->send();
+        }
+
+        // return
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success'),
+        ])->send();
     }
 
 
     // 取消收藏
-    public function cancelAction()
+    public function deleteAction()
     {
+        $id = $this->request->get('id', 'alphanum');
+        $type = $this->request->get('type', 'alphanum');
+
+        // check
+        if (!$id || !$type || !in_array($type, ['post'])) {
+            return $this->response->setJsonContent(['code' => 1, 'msg' => _('parameter error')])->send();
+        }
+
+        // delete
+        if (!$this->collectionModel->deleteCollection($type, $id, $this->uid)) {
+            return $this->response->setJsonContent(['code' => 1, 'msg' => _('fail')])->send();
+        }
+
+        // return
+        return $this->response->setJsonContent([
+            'code' => 0,
+            'msg'  => _('success'),
+        ])->send();
     }
 
 }
---
+++ app/v1/models/Collection.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace MyApp\V1\Models;
+
+
+use Phalcon\Mvc\Model;
+use Phalcon\DI;
+use Phalcon\Db;
+use MongoDB\BSON\ObjectId;
+
+class Collection extends Model
+{
+
+    public function addCollection($type = '', $id = '', $uid = '')
+    {
+        $mongodb = $this->di['mongodb'];
+        $db = $this->di['config']->mongodb->db;
+
+        if ($type == 'post') {
+            $data = $mongodb->$db->post->findOne(
+                ['_id' => new ObjectId($id)],
+                [
+                    'projection' => [
+                        '_id'     => 0,
+                        'uid'     => 1,
+                        'content' => 1,
+                        'picture' => 1,
+                        'voice'   => 1,
+                        'video'   => 1,
+                    ]
+                ]
+            );
+        }
+        if (!$data) {
+            return false;
+        }
+        $data['id'] = $id;
+
+        return $mongodb->$db->collection->updateOne(
+            ['_id' => new ObjectId($uid)],
+            [
+                '$addToSet'    => [$type => $data],
+                '$currentDate' => ['modifyTime' => true],
+            ],
+            ['upsert' => true]
+        );
+    }
+
+
+    public function deleteCollection($type = '', $id = '', $uid = '')
+    {
+        $mongodb = $this->di['mongodb'];
+        $db = $this->di['config']->mongodb->db;
+
+        return $mongodb->$db->collection->updateOne(
+            ['_id' => new ObjectId($uid)],
+            [
+                '$pull'        => [$type => ['id' => $id]],
+                '$currentDate' => ['modifyTime' => true],
+            ]
+        );
+    }
+
+
+    // TODO :: 分页
+    public function listCollection($type = '', $uid = '')
+    {
+        $mongodb = $this->di['mongodb'];
+        $db = $this->di['config']->mongodb->db;
+
+        $data = $mongodb->$db->collection->findOne(
+            ['_id' => new ObjectId($uid)],
+            [
+                'projection' => [$type => 1, '_id' => 0],
+            ]
+        );
+        return $data;
+    }
+
+}
