eb7a2830e827c0a99ad282da3ae198ea09406157
Joe <joe@xxtime.com>
2017-11-21 17:44:33 +0800

    路由规则 - 升级

--- app/bootstrap/routes.php
+++ app/bootstrap/routes.php
@@ -20,63 +20,30 @@ $router->removeExtraSlashes(true);
 
 
 // 通用路由
-$router->add(
-    '/:controller/:action/:params',
-    [
-        'controller' => 1,
-        'action'     => 2,
-        'params'     => 3
-    ]
-);
-
-$router->add(
-    '/:controller',
-    [
-        'controller' => 1
-    ]
-);
+$router->add('/:controller/:action/:params', ['controller' => 1, 'action' => 2, 'params' => 3]);
+$router->add('/:controller', ['controller' => 1]);
 
 
 // RESTFUL API
-$router->add(
-    '/(v[0-9]+)/:controller/:params',
-    [
-        'module'     => 1,
-        'controller' => 2,
-        'action'     => 'index',
-        'params'     => 3,
-    ]
-)->via(['GET']);
+$router->add('/(v[0-9]+)/:controller', ['module' => 1, 'controller' => 2, 'do' => 'get'])->via(['GET']);
+$router->add('/(v[0-9]+)/:controller', ['module' => 1, 'controller' => 2, 'do' => 'create'])->via(['POST']);
+$router->add('/(v[0-9]+)/:controller', ['module' => 1, 'controller' => 2, 'do' => 'update'])->via(['PUT']);
+$router->add('/(v[0-9]+)/:controller', ['module' => 1, 'controller' => 2, 'do' => 'delete'])->via(['DELETE']);
 
-$router->add(
-    '/(v[0-9]+)/:controller/:params',
-    [
-        'module'     => 1,
-        'controller' => 2,
-        'action'     => 'create',
-        'params'     => 3,
-    ]
-)->via(['POST']);
+$router->add('/(v[0-9]+)/:controller/:action', ['module' => 1, 'controller' => 2, 'action' => 3, 'do' => 'get'])->via(['GET']);
+$router->add('/(v[0-9]+)/:controller/:action', ['module' => 1, 'controller' => 2, 'action' => 3, 'do' => 'create'])->via(['POST']);
+$router->add('/(v[0-9]+)/:controller/:action', ['module' => 1, 'controller' => 2, 'action' => 3, 'do' => 'update'])->via(['PUT']);
+$router->add('/(v[0-9]+)/:controller/:action', ['module' => 1, 'controller' => 2, 'action' => 3, 'do' => 'delete'])->via(['DELETE']);
 
-$router->add(
-    '/(v[0-9]+)/:controller/:params',
-    [
-        'module'     => 1,
-        'controller' => 2,
-        'action'     => 'update',
-        'params'     => 3,
-    ]
-)->via(['PUT']);
+$router->add('/(v[0-9]+)/:controller/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'argv' => 3, 'do' => 'get'])->via(['GET']);
+$router->add('/(v[0-9]+)/:controller/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'argv' => 3, 'do' => 'create'])->via(['POST']);
+$router->add('/(v[0-9]+)/:controller/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'argv' => 3, 'do' => 'update'])->via(['PUT']);
+$router->add('/(v[0-9]+)/:controller/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'argv' => 3, 'do' => 'delete'])->via(['DELETE']);
 
-$router->add(
-    '/(v[0-9]+)/:controller/:params',
-    [
-        'module'     => 1,
-        'controller' => 2,
-        'action'     => 'delete',
-        'params'     => 3,
-    ]
-)->via(['DELETE']);
+$router->add('/(v[0-9]+)/:controller/:action/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'action' => 3, 'argv' => 4, 'do' => 'get'])->via(['GET']);
+$router->add('/(v[0-9]+)/:controller/:action/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'action' => 3, 'argv' => 4, 'do' => 'create'])->via(['POST']);
+$router->add('/(v[0-9]+)/:controller/:action/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'action' => 3, 'argv' => 4, 'do' => 'update'])->via(['PUT']);
+$router->add('/(v[0-9]+)/:controller/:action/([a-z0-9]{24})', ['module' => 1, 'controller' => 2, 'action' => 3, 'argv' => 4, 'do' => 'delete'])->via(['DELETE']);
 
 
 $router->setDefaultModule('v1');
